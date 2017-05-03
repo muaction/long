4b83d4fb17e0a87339657fa5688540d0ffc1eb7c
HungLM <hunglm.fth@gmail.com>
2017-05-04 00:35:33 +0700

    deploy: update file config.php and update db

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'longcar');
+define('DB_NAME', 'cp559386_longcar');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'cp559386_longcar');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'Long1234%');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'cpanel01wh-han1');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
